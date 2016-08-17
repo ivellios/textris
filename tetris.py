00c81ed6dc21c0640cdf2a92b0245b18286bb3ab
Janusz Kamieński <janusz.kamienski@proidea.org.pl>
2016-08-17 14:06:01 +0200

    moves are working fully, borders are checked, locking blocks changes them into part of the board -- few improvements would be nice, but it was not provided by the instructions

--- tetris.py
+++ tetris.py
@@ -1,18 +1,19 @@
 import os
 import sys
 import copy
+import random
 
-# clear the screen for drawing
+## some helper functions
 clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
 write = lambda x: sys.stdout.write(x)
 
 ALLOWED_MOVES = ['a', 'w', 's', 'd']
 
 BLOCKS = {
-    1: [(1,0), (1,0), (1,1)],
-    2: [(0,1), (0,1), (1,1)],
-    3: [(0,1), (1,1), (1,0)],
-    4: [(1,1), (1,1)],
+    0: [(1,0), (1,0), (1,1)],
+    1: [(0,1), (0,1), (1,1)],
+    2: [(0,1), (1,1), (1,0)],
+    3: [(1,1), (1,1)],
 }
 
 class UnexpectedInput(Exception):
@@ -21,77 +22,151 @@ class UnexpectedInput(Exception):
 class IllegalMove(Exception):
     pass
 
+class BlockLocked(Exception):
+    pass
+
 class Tetris(object):
     def __init__(self):
-        self.board = []## init empty board 20x20
+        self.board = [] ## init empty board 20x20 with contant parts
         for i in range(20):
             self.board.append([4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,])
         self.board.append([2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2])
-        self.block = 4
-        self.x = 1
-        self.y = 0
-        self.output_board = copy.deepcopy(self.board)
-        self.eog = False
+        self.output_board = copy.deepcopy(self.board) ## board for printing
+
+        self.eog = False # End of game?
+        # these will be set at the start of the game
+        self.block = None # Current block
+        self.x = 1 # Current x position of the block
+        self.y = 0 # Current y position of the block
+
+        self.temp_board = [] # temporary board for calculations
 
     ## operations on the blocks
-    def rotate_clockwise(self, block):
-        return zip(*block[::-1]) # TODO: check if valid (or move automatically)
+    def rotate_clockwise(self):
+        self.block = zip(*self.block[::-1])
 
-    def rotate_counter_clockwise(self, block):
-        return zip(*block)[::-1] # TODO: check if valid (or move automatically)
+    def rotate_counter_clockwise(self):
+        self.block = zip(*self.block)[::-1]
 
     def move_right(self):
-        self.x -= 1 # TODO: check if valid
+        self.x += 1
 
     def move_left(self):
-        self.x += 1 # TODO: check if valid
-
+        self.x -= 1
 
     def validate_input(self, move):
         if move not in ALLOWED_MOVES:
             raise UnexpectedInput, 'Your move is invalid \nValid moves are:\n a - left\n d - right\n s - rotate clockwise\n w - rotate counter clockwise)\n'
         return move
 
-    def play(self, valid_move = None):
-        self.calculate_board()
-        self.draw_board()
+    def make_move(self, valid_input):
+        """ Moves the block depending on the key input """
+        if valid_input == 'a':
+            self.move_left()
+        if valid_input == 'd':
+            self.move_right()
+        if valid_input == 'w':
+            self.rotate_counter_clockwise()
+        if valid_input == 's':
+            self.rotate_clockwise()
+        self.y += 1 # and at the same time it moves it down (if it is valid)
+        ## Probably it would be a good idea to displace block, check if it is
+        ## valid and then move down and again check. But the instructions didn't
+        ## consider it.
+
+    def revert_move(self, valid_input):
+        """ In case of exception reverts the movement of the block """
+        if valid_input == 'a':
+            self.move_right()
+        if valid_input == 'd':
+            self.move_left()
+        if valid_input == 'w':
+            self.rotate_clockwise()
+        if valid_input == 's':
+            self.rotate_counter_clockwise()
+        self.y -= 1
+
+    def generate_new_block(self):
+        self.block = copy.deepcopy(BLOCKS[random.randint(0,3)]) # pick a random block
+        self.y = 0 # set position to top and randomly on the x axis
+        self.x = random.randint(1,19)
+
+    def isLocked(self):
+        """ Tries to move down and checks whether the block is blocked by
+            anything below.
+        """
+        self.y += 1
+        try:
+            self.calculate_board(use_output = True) # uses output instead of contant board
+        except BlockLocked:
+            return True
+        finally:
+            self.y -= 1
+        return False
+
+    def play(self, valid_input = None):
+        self.generate_new_block() # create first block
+        self.calculate_board() # safe - at start will not raise any Exception
+        self.draw_board() # draw starting board
+        # and here we go...
         while not self.eog:
-            while not valid_move:
+            try:
+                valid_input = self.validate_input(raw_input('Make a move: '))
+                self.make_move(valid_input) # set the movement
+                self.calculate_board() # check the board for a new state and prere to be drawn
+                if self.isLocked(): # check if block is locked on place
+                    self.save_output_board() # make the block constant on board
+                    self.generate_new_block()
+                    try:
+                        self.calculate_board() # because, maybe there's no more space?
+                    except IllegalMove: # if so...
+                        self.draw_board() # draw the last state
+                        self.eog = True # and say goodbye.
+                        print("Game Over")
+                        break # bye bye...
+                self.draw_board()
+            except UnexpectedInput, e: # input key was unexpected
+                self.draw_board()
+                print(e)
+            except IllegalMove, e: # user cannot move brick to this position
+                self.revert_move(valid_input)
+                self.draw_board()
+                print(e)
+
+    def save_output_board(self): # so you say, block becomes part of the board?
+        for row in range(len(self.output_board)): # OK then...
+            for column in range(len(self.output_board[row])):
+                if self.output_board[row][column] == 1:
+                    self.output_board[row][column] = 2
+        self.board = copy.deepcopy(self.output_board)
+
+
+    def calculate_board(self, use_output = False):
+        # clone the board and let's place some block on it
+        ## unless you just want to make small stem ahead
+        self.temp_board = copy.deepcopy(self.output_board if use_output else self.board)
+        for row in range(len(self.block)):
+            for column in range(len(self.block[0])):
                 try:
-                    valid_move = self.validate_input(raw_input('Make a move: '))
-                    self.calculate_board(valid_move)
-                except UnexpectedInput, e:
-                    write(e)
-                except IllegalMove, e:
-                    write(e)
-            self.draw_board()
-            valid_move = None
-
-    def get_random_x(self):
-        return 5 ## TODO: randomize starting location
-
-    def calculate_board(self, valid_move = None):
-        if valid_move:
-            self.output_board = copy.deepcopy(self.board)
-            self.y += 1
-            if valid_move == 'a':
-                self.x -= 1
-            if valid_move == 'd':
-                self.x += 1
-        block = BLOCKS[self.block]
-        for i in range(len(block)):
-            for j in range(len(block[0])):
-                self.output_board[self.y+i][self.x+j] = self.board[self.y+i][self.x+j]+block[i][j]
-        if 5 in self.output_board:
+                    self.temp_board[self.y+row][self.x+column] = self.board[self.y+row][self.x+column]+self.block[row][column]
+                except IndexError: # your block is out of the array
+                    raise IllegalMove, 'Cannot rotate that way it moves out of border'
+        if any(5 in row for row in self.temp_board): # you stepped on the border
             raise IllegalMove, 'Cannot move on the border'
-        if 3 in self.output_board:
-            pass
+        if not use_output:
+            if any(3 in row for row in self.temp_board): # you stepped on the bottom (incl. other locked blocks)
+                raise IllegalMove, "Cannot move block here"
+            self.output_board = copy.deepcopy(self.temp_board) # temp is OK? then we keep it to be drawn soon
+        if any(3 in row for row in self.temp_board): # your block stepped on something in this __TRY__.
+            raise BlockLocked
 
     def draw_board(self):
+        # Let's make it clear...
         clear()
-        for i in range(21): # for each row
-            for j in range(22): # for each column
-                write(str(self.output_board[i][j]) if self.output_board[i][j] > 0 else ' ') # FIXME: change print to *
+        # The board will be drawn now:
+        for row in range(21):
+            for column in range(22):
+                write('*' if self.output_board[row][column] > 0 else ' ')
             write('\n')
 
 if __name__=='__main__':
